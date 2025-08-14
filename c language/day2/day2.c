23620e9755356c8929b68bf0f4be969637941dba
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-08-14 10:44:37 +0530

    day2

---
+++ c language/day2/day2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+
+  printf("           All students result     \n");
+  printf("\n");
+
+    printf("=========================================\n");
+    
+    printf("Roll.No.  Name   P   C   M    E    CS    %%age      Grade\n");
+    printf("=========================================\n");
+
+  return 0;
+}
