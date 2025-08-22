43ce0ceb24f4b3d1b86da1d4b3b05baadf781cfe
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-08-22 11:02:25 +0530

    day5

---
+++ c language/day5operators/day5.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int main()
+{
+
+  // int num = 1;
+  // // postfix
+  // num++; // 2
+
+  // int store = num++; // first assing the value then increament value in postfix  2 will be assign then it increment  is 3
+
+  // // prefix
+
+  // ++num; // 4
+
+  // int store2 = ++num; // 5   first increment the value then assgin the value  to the variable
+
+  // printf("%d\n", num);    // 5
+  // printf("%d\n", store);  // 2
+  // printf("%d\n", store2); // 5
+  // printf("------------------------------\n");
+
+  // int value1 = 13;
+
+  // int value2 = 2;
+
+  // // int result = value1 / value2;// return quetient =6
+  // int result = value1 % value2; // return remainder =0
+
+  // printf("%d\n", result);
+
+  // int sum = 0;
+  // int value;
+
+  // sum = sum + 5;
+
+  // sum %= 5;
+
+  // printf("sum is %d", sizeof sum);
+
+  // printf("enter your number:");
+  // scanf("%d", &value);
+
+  // printf("value is %d", value);
+
+  int result = (13 + 5) - 2;
+
+  printf("%d", result);
+
+  return 0;
+}
