6f2077df8bcbef634e116c8807bd535e8b290154
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-08-25 10:40:04 +0530

    day6

---
+++ c language/day6control/day6.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+
+int main()
+{
+
+  // int score = 90;
+
+  // // if(condition){
+  // //   // execution
+  // // }
+
+  // if (score > 80) // true
+  // {
+  //   printf("you will get bike \n");
+  // }
+
+  // if (score > 90) // false
+  // {
+  //   printf("you will get bike \n ");
+  // }
+  // else
+  // {
+  //   printf("will get pitai \n");
+  // }
+
+  // int num1 = 5;
+  // int num2 = 5;
+
+  // if (num1 != num2)
+  // {
+  //   printf("num2 = %d is greater than num1 ", num2);
+  // }
+  // else
+  // {
+  //   printf("num1 = %d is greater than num2 ", num1);
+  // }
+
+  // char light = 'G';
+
+  // if (light == 'R')
+  // {
+  //   printf("stop\n");
+  // }
+  // else if (light == 'Y')
+  // {
+  //   printf("get ready \n");
+  // }
+  // else if (light == 'G')
+  // {
+  //   printf("go \n");
+  // }
+  // else
+  // {
+  //   printf("invalid lgiht \n");
+  // }
+
+  // int budget = 4000;
+
+  // if (budget >= 9000)
+  // {
+  //   printf(" taj");
+  // }
+  // else if (budget >= 6000)
+  // {
+  //   printf(" patang");
+  // }
+  // else if (budget >= 2000)
+  // {
+  //   printf("freezbee");
+  // }
+  // else if (budget >= 2000)
+  // {
+  //   printf("TRP");
+  // }
+  // else
+  // {
+  //   printf("ghar betho");
+  // }
+
+  int a = 15;
+  int b = 30;
+
+  a = a + b;
+
+  b = a - b;
+
+  a = a - b;
+
+  // a= b;
+
+  // b = a;
+
+  printf("a=%d\n",a);
+    printf("b=%d\n",b);
+
+      return 0;
+}
