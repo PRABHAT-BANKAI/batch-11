9960d29f251fd96b51f315ab11353f79e7bd89ac
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-08-26 10:38:04 +0530

    nested if else

---
+++ c language/day7nestedifelse/day7.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main()
+{
+
+  // int a, b, c;
+
+  // a = 100;
+  // b = 100;
+  // c = 100;
+
+  // if (a == b && b == c && c == a)
+  // {
+  //   printf("all are same\n");
+  //   return 0;
+  // }
+
+  // if (a > b)
+  // {
+  //   if (a > c)
+  //   {
+  //     printf("a is maximum\n");
+  //   }
+  //   else
+  //   {
+  //     printf("c is maximum\n");
+  //   }
+  // }
+  // else
+  // {
+  //   if (b > c)
+  //   {
+  //     printf("b is maximum\n");
+  //   }
+  //   else
+  //   {
+  //     printf("c is maximum\n");
+  //   }
+  // }
+
+  int email, password;
+
+  email = 123;
+  password = 678;
+
+  if (email == 123 && password == 678)
+  {
+    printf("login successfully");
+  }
+  else
+  {
+    printf("email and password is not correct ");
+  }
+
+  return 0;
+}
