fa79cbcfd5b44fcf3b6922e6b734fe98b0355bcd
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-08-28 11:22:53 +0530

    loop

---
+++ c language/day9loop/day9.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main()
+{
+
+  // printf("1");
+  // printf("2");
+  // printf("3");
+  // printf("4");
+  // printf("5");
+  // printf("6");
+
+  // syntax
+  //  for(intialization;condition; inc or dec){
+
+  // }
+
+  // for (int i = 5; i >= 1; i--)
+  // {
+
+  //   printf("%d\n", i);
+  // }
+
+  // while loop
+
+  // initialzization
+
+  // while (condition ){
+
+  //   inc or dec
+  // }
+
+  int i = 5;
+
+  while (i >= 1)
+  {
+
+    printf("%d\n", i);
+    i--;
+  }
+
+  return 0;
+}
+
