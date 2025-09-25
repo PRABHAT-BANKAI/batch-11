765592bf2a49204b6318e8a88f1d86b50fce66e6
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-09-25 10:32:35 +0530

    c++ day1

---
+++ Cplus/day1/day1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+  string name;
+
+  int a, b;
+  a = 10;
+  b = 24;
+
+  int result = a + b;
+
+  cout << "Enter your Name ";
+  cin >> name;
+
+  cout << "userName:" << name<<result;
+
+  return 0;
+}
