769eb6c5bdb29e2ab9575dc937931a929539da0d
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-11-06 10:47:05 +0530

    day2

---
+++ JavaScript/day2/index.js
@@ -0,0 +1,93 @@
+// what is javascript
+// is a scripting language that enables you to create dynamically updating content, control multimedia, animate images, and pretty much everything else. (Okay, not everything, but it is amazing what you can achieve with a few lines of JavaScript code.)
+
+// data types
+
+//primitive data type
+// number (123)
+// string ("hello world")
+// boolean (true /false)
+// undefined
+// bigint
+// null
+
+// non primitive data type (reference type)
+// function
+// object
+// array
+
+// let var const
+
+let num1 = 123;
+console.log(num1);
+
+let str = "hello";
+console.log(str);
+
+let boolean = 25 < 5;
+
+console.log(boolean);
+
+let userName = "name";
+
+
+let gameName = null
+console.log(gameName)
+
+//function // block of code 
+
+
+function show(){
+  console.log("hello javascript")
+}
+
+
+show()
+
+
+//object  is a key value pairs
+
+
+let yuvraj = {
+  userName:"yuvraj",
+  age:22,
+  address:"gujarat"
+}
+
+console.log(yuvraj)
+
+
+// array where we can store multiple values of data types 
+
+
+let arr = [12,true,"hello",12]
+
+console.log(arr)
+console.log(arr[2])
+
+
+
+let studentName = "owais"
+let digit = "213"
+
+let temp = true
+
+console.log(typeof studentName)
+console.log(typeof(digit))
+console.log(typeof typeof temp)
+
+
+
+
+let laptopName = "acer"
+console.log(laptopName)
+laptopName = "asus"
+
+laptopName = 12356 // dynamically update
+laptopName = false
+
+console.log(laptopName)
+
+
+
+
---
+++ JavaScript/day2/readme.md
@@ -0,0 +1,14 @@
+Q1. Print "Your Name" in the console   in next line "Print your Strength".
+
+Q2. Define a variable called name with your Name as the assigned value
+Print the value stored in the variable name
+Change the variable to store your father's name
+Print the value stored in the variable name
+Change the variable again to store your mother's name.
+Print the value stored in the variable name
+Q3. Create a variable name to store your name
+Create a variable age to store your age
+Concatenate and console them in a new string to get the output like this:
+("My name is"+ name +"and my age is"+ age +".")
+Q4. Store the name, school, grade, section, roll no and the marks scored by the student in 3 subjects
+       Print the report card of the student (You can make it look nice by using some keyboard symbols)
