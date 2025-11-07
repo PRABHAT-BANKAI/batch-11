718fd38cbe64bc04f42c6e0bf067a6afaaed32f8
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-11-07 10:44:51 +0530

    day3

---
+++ JavaScript/day3/index.js
@@ -0,0 +1,80 @@
+// Types Of Operator
+// - Arithmetic operators
+// - Assignment operators
+// - Comparison operators
+// - Logical operators
+// - conversion with console.log and innerHTML"
+
+//bigint
+
+// const number = 123787879872349798237498393n+12n
+
+// console.log(number)
+
+// - Arithmetic operators
+
+let a = 15;
+let b = 20;
+
+let result = a + b;
+console.log(a + b);
+
+let subResult = a - b;
+console.log(subResult);
+
+let multResult = a * b;
+console.log(multResult);
+
+let number1 = 2;
+
+console.log(number1 ** 5); //exponention(Power)
+
+let sum = 10;
+
+sum = sum + 4;
+sum += 5;
+
+console.log(sum);
+
+// comparison operator
+
+console.log(5 == 5);
+console.log(5 == "5"); //equal to
+
+//equal value and equal type
+console.log(5 === "5"); //strickly equal to
+
+console.log(4 === 4);
+
+//!=	not equal
+
+console.log(4 !== "4");
+
+// >
+// <
+// >=
+// <=
+
+console.log(5 < 5);
+console.log(6 > 3);
+console.log(4 >= 1);
+console.log(3 <= 9);
+
+// logical operators
+// and &&
+// or ||
+
+console.log(5 < 6 && 5 == 3 && 3==3);
+
+console.log(5 < 6 || 5 == 3 || 3==3);
+
+
+console.log(12>5 && 45==3|| 55==55 && 3<1 || 33!=33)
+//false ||false||true
+
+
+console.log(true && true && true)
+
+
+
+console.log(Math.random(),"number")
