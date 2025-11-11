a9f9886ad6dd7112b695b1312840cdeeb7b092dc
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-11-11 10:52:17 +0530

    q3

---
+++ JavaScript/day3/conversion.js
@@ -0,0 +1,35 @@
+// implicit explicit
+
+//implicit /// corciean// javascript handle by own 
+
+console.log("12" - 5);
+
+console.log("12" + true); // concatenate
+
+console.log("155" + 44 + 11);
+console.log(155 + 11 + "33");
+
+console.log(15 - false);
+
+//explicit// conversion // manually changes with data types 
+
+let number = "155a";
+console.log(typeof number);
+
+const result = Number(number);
+console.log(result);
+
+//String()
+
+let boolean = false;
+let result2 = Number(boolean);
+console.log(result2);
+
+// "">false
+
+//Boolean()
+
+let string = " ";
+
+let result3 = Boolean(string);
+console.log(result3);
---
+++ JavaScript/day4/index.js
@@ -0,0 +1,46 @@
+//swapping
+
+let a = 10;
+let b = 15;
+
+// b=  10
+//a = 15
+
+// let temp ;
+// temp = a
+
+// a = b
+
+// b= temp
+
+// console.log(a)
+// console.log(b)
+
+a = a + b;
+b = a - b;
+a = a - b;
+
+console.log(a);
+console.log(b);
+
+let english = 55;
+let hindi = 88;
+let html = 33;
+
+const total = english + hindi + html;
+
+const result = total / 3;
+
+console.log(result);
+console.log(Math.floor(result));
+console.log(Math.ceil(result));
+
+
+console.log(Math.round(3.4))
+
+console.log(Math.floor(Math.random()*100))
+
+
+console.log((12-5)+(25*5)*2-7+(5+5))
+//7+125*2-7+10
+7+250-7+10
