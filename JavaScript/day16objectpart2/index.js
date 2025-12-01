6efaf1915b0a33ff58e6ca1a84c952d732bec1ee
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-12-01 10:47:59 +0530

    arrayobjectwithhtml

---
+++ JavaScript/day16objectpart2/index.js
@@ -0,0 +1,19 @@
+	
+// Object with Array Objects Looping Array with object access with looping and conditions Make Quotes"
+
+
+// let arr = [1,2,3]// ref #@8571
+
+// let arr2 = [1,2,3] // ref #@12351
+
+// let arr3 = arr//// ref #@8571
+
+ 
+// arr3[3] = 15
+
+// console.log(arr == arr3)
+
+
+// console.log(arr)
+
+
